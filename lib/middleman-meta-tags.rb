cc9c0413d50749892884b73cccd0fd7287a3317e
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2014-03-22 15:58:39 +0100

    Structuring gem

--- lib/middleman-meta-tags.rb
+++ lib/middleman-meta-tags.rb
@@ -1,4 +1,6 @@
-module Middleman
-  module MetaTags
-  end
+require 'middleman-core'
+
+::Middleman::Extensions.register(:meta_tags) do
+  require 'middleman-meta-tags/extension'
+  ::Middleman::MetaTags
 end
---
+++ lib/middleman-meta-tags/helpers.rb
@@ -0,0 +1,6 @@
+module Middleman
+  module MetaTags
+    module Helpers
+    end
+  end
+end
