7ac7c50503d904c1f44e90521ed1d0fe8b04bcbd
Will Schenk <wschenk@gmail.com>
2014-11-20 14:46:49 -0500

    Added title as symbol

--- lib/middleman-meta-tags/helpers.rb
+++ lib/middleman-meta-tags/helpers.rb
@@ -57,11 +57,11 @@ module Middleman
       def auto_tag
         site_data = data['site'] || {}
 
-        fall_through( site_data, "title", "title" )
 
         set_meta_tags :site => site_data['title']
         set_meta_tags "og:site_name" => site_data['title']
 
+        fall_through( site_data, :title, "title" )
         fall_through( site_data, :description, "description" )
 
         # Twitter cards
