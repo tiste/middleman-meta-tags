70dfcec675adc1b6ca62bba365f98620f19e355a
Piotr Usewicz <piotr@layer22.com>
2015-12-19 12:10:44 +0100

    Update extension.rb

--- lib/middleman-meta-tags/extension.rb
+++ lib/middleman-meta-tags/extension.rb
@@ -2,9 +2,6 @@ require 'middleman-meta-tags/helpers'
 
 module Middleman
   class MetaTagsExtension < Extension
-    def initialize(app, options = {}, &block)
-      super
-      app.helpers Middleman::MetaTags::Helpers
-    end
+    self.defined_helpers = [ Middleman::MetaTags::Helpers ]
   end
 end
